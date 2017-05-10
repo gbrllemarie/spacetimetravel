0f4d414106776c4632464fb5a5df211f0f5c44b5
Gabby Torres <torres.gabriellemarie@gmail.com>
2017-05-10 13:36:53 +0000

    minor code refactors, move compiler file to /src/

--- bin/spacetime.py
+++
@@ -1,2 +0,0 @@
-
-# compiler and runner program for spacetime travel source files
---
+++ src/spacetime.py
@@ -0,0 +1,4 @@
+
+# compiler and runner program for spacetime travel source files
+# should call ./main.py (parses input source code and outputs C translation)
+# gcc compilation should happen here
