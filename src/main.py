d733c6ed4ba33dfeb6d8fc57d29d96599ce2c021
Arielle Gabriel <ariellegabriel96@gmail.com>
2017-05-20 11:23:32 +0800

    fixed if, for, samples files

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 import sys, os, re
 from plex import *
 from plex.traditional import re as rex
+import re as regex
 from StringIO import *
 
 # -- File Input Validation ----------------------------------------------------#
@@ -30,9 +31,9 @@ ident_strip = Range("AZaz") + Rep(Range("AZaz09"))
 identifier  = wspace + Str(":") + ident_strip + Str(":") + wspace
 integer     = Rep1(Range("09"))
 number      = integer + (Str(".") + Rep1(Range("09")) | Empty)
-dtype       = Str("numeral")|Str("decimal")|Str("star")|Str("constellation")|Str("day")|Str("vacuum")
-equality_operators = Str(">") | Str("<") | Str(">=") | Str("<=") | Str("==") | Str("!=")
-check_token = wspace + Str("(")+ Rep(Range("AZaz09")) + wspace + equality_operators + wspace + Rep(Range("AZaz09")) + Str(")")
+dtype       = Str("numeral")|Str("decimal")|Str("starz")|Str("constellation")|Str("day")|Str("vacuum")
+equality_operators = Str("?>") | Str("?<") | Str("?>=") | Str("?<=") | Str("?=") | Str("?!=")
+# check_token = wspace + Str("(")+ Rep(Range("AZaz09")) + wspace + equality_operators + wspace + Rep(Range("AZaz09")) + Str(")")
 
 # -- generate arrays for lexicon --------------------------#
 # reserved words
@@ -46,11 +47,11 @@ lex_reserved = [
       # syntax
     ( Str("loop"),              "syntax_for"        ), # for loop
     ( Str("cycle"),             "syntax_while"      ), # while loop
-    ( Str("check") + check_token,             "syntax_if"         ), # if
-    ( Str("recheck") + check_token,           "syntax_elseif"     ), # else if
+    ( Str("start check")+wspace+lex_expr_paren_newline,       "syntax_if"         ), # if
+    ( Str("recheck")+wspace+lex_expr_paren_newline,           "syntax_elseif"     ), # else if
     ( Str("retreat"),           "syntax_else"       ), # else
     ( Str("receive") + identifier,           "syntax_scanf"       ), # scanf
-    ( Str("display") + identifier,           "syntax_printf"       ), # printf
+    ( (Str("display")|Str("displayln")) + identifier,           "syntax_printf"       ), # printf
       # helpers
     ( Str("tick") + lex_expr_paren_newline,              "helper_increment"  ), # ++
     ( Str("tock") + lex_expr_paren_newline,              "helper_decrement"  ), # --
@@ -60,7 +61,7 @@ lex_reserved = [
     # ( Str("OMEGA"),             "block_mainfnend"   ), # function end
     # ( Str("activate"),          "block_fnstart"     ), # function start
     # ( Str("deactivate"),        "block_fnend"       ), # function end
-    ( Str("start"),             "block_start"       ), # block start
+    # ( Str("start check"),       "block_start_if"       ), # block start
     ( Str("end check"),         "block_end_if"         ), # block end check
     ( Str("end"),               "block_end"         ), # block end
     ( Str("walangforever"),     "block_break"       ), # break
@@ -78,12 +79,12 @@ lex_datatypes = [
     # data types
     ( Str("numeral"),           "datatype_int"      ), # int datatype
     ( Str("decimal"),           "datatype_float"    ), # float datatype
-    ( Str("star"),              "datatype_char"     ), # char datatype
+    ( Str("starz"),              "datatype_char"     ), # char datatype
     ( Str("constellation"),     "datatype_string"   ), # string (char[n]) datatype
     ( Str("day"),               "datatype_bool"     ), # bool datatype
 ]
 
-vardec_token = ( Str("numeral", "decimal", "star", "day", "constellation")
+vardec_token = ( Str("numeral", "decimal", "starz", "day", "constellation")
                + wspace + identifier +Opt(Str("<")+integer+Str(">"))+ Eol )
 lex_vars = [
     ( vardec_token,             "syntax_vardec"     ), # variable declaration
@@ -106,10 +107,9 @@ fncall_token = (Str("warp(")
                       + Rep(Str(",") + Rep(wspace + (number|identifier) + wspace)
                       )
                )+Str(")")+Eol)
-fnhead_token = Str("activate")+identifier+Str("with ")+Str("(")+ wspace+Rep(Opt(dtype+Alt(number, identifier)
-            + Rep(Str(",") + Rep(wspace+dtype+wspace + (number|identifier) + wspace)
-            ))
-            )+ Str(")") + wspace + Opt(Str("returns")+wspace+dtype)
+fnhead_token = Str("activate")+identifier+Str("with (")+ wspace+Opt(dtype+Alt(number, identifier)
+            + Rep(Str(",") + wspace+dtype+wspace + (number|identifier) + wspace)
+            ) + Str(")") + wspace + Opt(Str("returns")+wspace+dtype)
 lex_fncall = [
     ( fncall_token,                         "helper_subprogram" ), # add functional calls
     ( Str("transmit")+wspace+Alt(identifier|number),"helper_return" ), # add function return
@@ -118,7 +118,7 @@ lex_fncall = [
 ]
 
 # expressions
-lex_expr_paren = Str("(")+Rep(AnyBut(""))+Str(")")+wspace
+lex_expr_paren = Str("(")+Rep(AnyBut("\n"))+Str(")")+wspace
 lex_expr_op = Alt(identifier|number)+Rep(wspace+Any("+-*/")+wspace+Alt(identifier|number))
 lex_expr = [
     ( lex_expr_paren,           "expr_paren"        ), # parenthesis
@@ -168,19 +168,19 @@ lex_error = [
 #    ( mainfn_end_token,         "block_mainfnend"   ),
 #]
 
-#for_condition = Str ("(since") + EXPRESSION + wspace + Str("until") + EXPRESSION + wspace +Str("do") TICK OR TOCK + Str(")")
-#lex_for = [
-#    ( Str("start loop"), "syntax_for"),
-#    ( Str("end loop"), "syntax_for_end"),
-#    ( for_condition, "syntax_for_condition"),
-#]
+for_condition = Str ("since ") + Rep(AnyBut("")) + Str(" until ") + Rep(AnyBut("")) +Str(" do ") + Rep(AnyBut("\n"))
+lex_for = [
+   ( Str("start loop"), "syntax_for"),
+   ( Str("end loop"), "syntax_for_end"),
+   ( for_condition, "syntax_for_condition"),
+]
 
-#while_condition = Str ("(until") + EXPRESSION + Str(")")
-#lex_while = [
-#    ( Str("start cycle"), "syntax_while"),
-#    ( Str("end cycle"), "syntax_while_end"),
-#    ( while_condition, "syntax_while_condition"),
-#]
+while_condition = Str ("(until") + Rep(AnyBut("")) + Str(")\n")
+lex_while = [
+   ( Str("start cycle"), "syntax_while"),
+   ( Str("end cycle"), "syntax_while_end"),
+   ( while_condition, "syntax_while_condition"),
+]
 
 # -- generate the lexicon ---------------------------------#
 # put everything together
@@ -190,9 +190,14 @@ lex_tokens += lex_expr
 lex_tokens += lex_comments
 lex_tokens += lex_vars
 lex_tokens += lex_formatting
+lex_tokens += lex_for
 # lex_tokens += lex_misc
 lex_tokens += lex_fncall
 lex_tokens += lex_error
+
+lex_paren = lex_for
+lex_paren += [(lex_expr_op,"expr_op")]
+
 
 lexicon = Lexicon(lex_tokens)
 
@@ -206,17 +211,17 @@ def getName(str1):
     return str1.replace(':', '')
 
 def getDtype(type):
-    list_type =  {'numeral':'int', 'decimal': 'float', 'star':'char', 'constellation': 'char *','day':'bool'}
+    list_type =  {'numeral':'int', 'decimal': 'float', 'star':'char', 'constellation': 'char *','day':'bool','vacuum':'void'}
     return list_type[type]
 
-def parseFns(token):
+def translateFns(token):
     fxn_name = token[1].split(" ")[1][1:-1]
     print token[1].split(" ")
     print token[1].split(" ")[1][1:-1]+"()"
 
-def parseVardec(token):
-    varname = token.split(':')[1]
-    scan1 = Scanner(Lexicon(lex_datatypes), StringIO(token), source_output)
+def addVarDataType(var):
+    varname = var.split(':')[1]
+    scan1 = Scanner(Lexicon(lex_datatypes), StringIO(var), source_output)
     tok = scan1.read()
     if tok[0] == "datatype_int":
         print "int "+str(varname),
@@ -234,6 +239,10 @@ def parseVardec(token):
         print "bool "+str(varname),
         setDictVarAndType(varname, "bool")
 
+def translateVardec(token):
+    varname = token.split(':')[1]
+    addVarDataType(token)
+
     temp = filter(None,token.split(':'))
     if(len(temp) > 2):
         print "["+temp[2][1:-1]+"]",
@@ -247,7 +256,7 @@ def typeof(variable):
     variable_type = variable_types_dict[variable]
     return variable_type
 
-def parseScanInput(token):
+def translateScanInput(token):
     varname = token.split(':')[1].strip()
     format_code = ""
 
@@ -271,23 +280,30 @@ def parseScanInput(token):
     
     return c_scan_string
 
-def parsePrintInput(token): 
-    varname = token.split(':')[1].strip()
+def translatePrintInput(token):
+    tok_split = token.split(':')
+    varname = tok_split[1].strip()
     format_code = ""
+    newline = ""
+    if(tok_split[0].strip(' ') == 'displayln'):
+        newline = "\\n"
+    try:    
+        if typeof(varname) == "int":
+            format_code = "%d"
+        elif typeof(varname) == "float":
+            format_code = "%.2f"
+        elif typeof(varname) == "char": 
+            format_code = "%c"
+        elif typeof(varname) == "char-array": 
+            format_code = "%s"
 
-    if typeof(varname) == "int":
-        format_code = "%d"
-    elif typeof(varname) == "float":
-        format_code = "%.2f"
-    elif typeof(varname) == "char": 
-        format_code = "%c"
-    elif typeof(varname) == "char-array": 
-        format_code = "%s"
-
-    printf = 'printf("'
-    c_print_string = printf + format_code + '", ' + varname + ");"
-    
-    return c_print_string
+        printf = 'printf("'
+        c_print_string = printf + format_code + newline + '", ' + varname + ");"
+        
+        return c_print_string
+    except:
+        print "\nERROR: Variable "+varname+" has no datatype or has not been declared"
+        exit()
 
 def increment_or_decrement(token, incr_decr_concat):
     varname = token.split(':')[1].strip()
@@ -298,24 +314,24 @@ def increment_or_decrement(token, incr_decr_concat):
         return "Error occured with helper"
     return varname
 
-def parseFnheader(token):
+def translateFnheader(token):
     # 0 - activate, 1 - fname, 2 - with, 3 - args, -2- returns, -1 - return type
     tok = token.split(' ')
     fname = getName(tok[1])
     args = (' '.join(tok[3:-2])).strip('()')
     args = filter(None,args.split(','))
     return_type = tok[-1]
-
     print getDtype(return_type)+" "+fname+" (",
     for i in range(len(args)):
         if i > 0:
             print ", ",
-        temp = filter(None,args[i].split(' '))
-        print getDtype(temp[0])+" "+getName(temp[1]),
+        addVarDataType(args[i].strip(' '))
+        # temp = filter(None,args[i].split(' '))
+        # print getDtype(temp[0])+" "+getName(temp[1]),
     print ") {",
 
 
-def parseFncall(token):
+def translateFncall(token):
     tok = filter(None,re.split('\(|\)', str(token)))
     args = ""
     if(len(tok) > 2):
@@ -329,31 +345,65 @@ def parseFncall(token):
     return
 
 
-def parseFnreturn(token):
+def translateFnreturn(token):
     tok = token.split(' ')
     tok[1] = tok[1].replace(':', '')
     print "return "+tok[1]+";"
 
-def parseExprOp(token, end=1):
+def translateExprOp(token, end=1):
     token = token.replace(":","")
     if end == 1:
         token = token+";"
     return token
 
-def parseExprParen(token):
+def translateExprParen(token):
     strip_token = token[1:-1] #remove enclosing parentheses
-    scan1 = Scanner(Lexicon(lex_expr), StringIO(strip_token), source_output)
+    scan1 = Scanner(Lexicon(lex_paren), StringIO(strip_token), source_output)
     tok = scan1.read()
-    #if tok[0] == ""
+    trans = "("
+    if tok[0] == 'syntax_for_condition':
+        temp = tok[1].replace("since", "")
+        temp = temp.replace(" until", ";")
+        temp = temp.replace(" do", ";")
+        temp1 = temp.split(";")
+        for i in temp1:
+            trans += translateVarAssign(i) + "; "
+        trans = trans.strip("; ")
+        trans += ") {"
+    else:
+        temp = tok[1]
 
-def parseIfElseIfElseClause(spacetime_syntax, c_syntax, token):
+    return trans
+
+def translateVarAssign(token):
+    temp = token.replace("?=", "==")
+    temp = temp.replace("?|","||")
+    temp = temp.replace("?&","&&")
+    temp = temp.replace("mod","%")
+    temp = temp.replace("?","")
+    temp = temp.replace(":","")
+    temp = temp.replace("<-","=")
+    return temp
+
+
+def translateIfElseIfElseClause(spacetime_syntax, c_syntax, token):
     c_if_syntax = token.replace(spacetime_syntax, c_syntax)
-    c_if_syntax = c_if_syntax + " {"
-    return c_if_syntax    
+    c_if_syntax = c_if_syntax.replace("mod", "%")
+    c_if_syntax = c_if_syntax.replace("?=", "==")
+    c_if_syntax = c_if_syntax.replace("?|", "||")
+    c_if_syntax = c_if_syntax.replace("?&", "&&")
+    c_if_syntax = c_if_syntax.replace("?", "")
+    c_if_syntax = c_if_syntax.replace("<-", "=")
+    c_if_syntax = c_if_syntax.replace(":", "")
+    c_if_syntax = c_if_syntax + "{"
+    return c_if_syntax
+
+# def trans
 
 # read through the source input until EOF
 linenum = 1
-preprocess = "#include <stdio.h>\n#include <stdlib.h>\n"
+preprocess = "#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>\n#include <stdbool.h>\n"
+preprocess += "#define darkness false\n #define light true"
 print preprocess
 while 1:
     token = scanner.read()
@@ -391,24 +441,28 @@ while 1:
     # -- syntax
     elif token[0] == "syntax_for":
         print "for",
+    elif token[0] == "syntax_for_condition":
+        pass
+    elif token[0] == "syntax_for_end":
+        print "}"
     elif token[0] == "syntax_while":
         print "while",
     elif token[0] == "syntax_if":
-        print parseIfElseIfElseClause("check", "if", token[1])
+        print translateIfElseIfElseClause("start check", "if", token[1])
     elif token[0] == "syntax_elseif":
-        print parseIfElseIfElseClause("recheck", "else if", token[1])
+        print translateIfElseIfElseClause("recheck", "else if", token[1])
     elif token[0] == "syntax_else":
-        print parseIfElseIfElseClause("retreat", "else", token[1])
+        print translateIfElseIfElseClause("retreat", "else", token[1])
     elif token[0] == "block_end_if":
         print "}"
     elif token[0] == "syntax_comment":
         print "/*" + token[1][2:-2] + "*/",
     elif token[0] == "syntax_vardec":
-        parseVardec(token[1])
+        translateVardec(token[1])
     elif token[0] == 'syntax_scanf': 
-        print parseScanInput(token[1])
+        print translateScanInput(token[1])
     elif token[0] == 'syntax_printf': 
-        print parsePrintInput(token[1])
+        print translatePrintInput(token[1])
     elif token[0] == 'syntax_varassign':
         tok = token[1].replace(":","")
         tok = tok.replace("<-"," =")
@@ -424,11 +478,11 @@ while 1:
     elif token[0] == "helper_typecast":
         print "(typecast here)",                       # TODO
     elif token[0] == "helper_subprogram":
-        parseFncall(token[1])
+        translateFncall(token[1])
     elif token[0] == "helper_return":
-        parseFnreturn(token[1])
+        translateFnreturn(token[1])
     elif token[0] == "helper_fnheader":
-        parseFnheader(token[1])
+        translateFnheader(token[1])
     elif token[0] == "helper_fnfooter":
         print "}"
 
@@ -449,10 +503,10 @@ while 1:
     elif token[0] == "block_fnend":
         print "}",
         #fileout.write("}\n")
-    elif token[0] == "block_start":
+    elif token[0] == "block_start_if":
         print "{",
         #fileout.write("{\n")
-    elif token[0] == "block_end":
+    elif token[0] == "block_end_if":
         print "}",
         #fileout.write("}\n")
     elif token[0] == "block_break":
@@ -470,13 +524,13 @@ while 1:
     elif token[0] == "formatting_newline":
         print "\n",
     elif token[0] == "expr_paren":
-        print token,
+        print translateExprParen(token[1])
     elif token[0] == "expr_addsub":
         print token,
     elif token[0] == "expr_muldiv":
         print token,
     elif token[0] == "expr_op":
-        print parseExprOp(token[1])
+        print translateExprOp(token[1])
 
     # Insert more todos here
     else:
